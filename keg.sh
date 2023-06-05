cc658bdac5602cc2b5f7363315702d2e0dcbf6ee
Kyle LeBlanc <kyle@leblanc.sh>
2023-06-05 16:16:34 -0400

    feat: Update Main Case
    -Update prompt to be more concise
    -Adjust Uninstall Pkg option to prompt user to confirm, and only allow one uninstall selection

--- keg.sh
+++ keg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #################################
-# Keg - easily manage Homebrew  #
+# KeG - easily manage Homebrew  #
 # Copyright Jacob Stordahl 2023 #
 #################################
 #
@@ -13,33 +13,45 @@ if ! which -s gum > /dev/null 2>&1 || ! which -s brew > /dev/null 2>&1; then
     exit 1
 fi
 
-gum style --foreground="#f5c2e7" "Welcome to Keg! What would you like to do?"
+gum style --foreground="#539FC5" "Welcome to KeG! What would you like to do?"
 
-TASK=$(echo "Install a package with Brew\nUpdate Brew\nUpdate a Brew Package(s)\nUninstall a Brew Package(s)" | gum choose --limit 1)
+TASK=$(echo "Install a Package\nUpdate Brew\nUpdate a Brew Package(s)\nUninstall Package" | gum choose --limit 1)
 
 case "$TASK" in
-  "Install a package with Brew") 
-    P=$(gum input --placeholder "Package name")
-    gum spin --spinner dot --title "Installing $P" -- brew install $P
-    echo "$P is installed!"
-  ;;
-  "Update Brew") 
-    gum spin --spinner dot --title "Updating brew (this may take a while)" -- brew update
-    echo "Brew is updated!"
-  ;;
-  "Update a Brew Package(s)")
-    echo "Space to select, Enter to update"
-    L=$(brew list | gum choose --no-limit)
-    for p in `echo $L` 
+    "Install a Package")
+    NAME=$()
+    gum spin --spinner dot --title "Installing $NAME" -- brew install $NAME
+    echo "$NAME was installed!"
+    ;;
+
+    "Update Brew")
+    gum spin --spinner dot --title "Updating brew. Please Wait..." -- brew update
+    VERSION=$(brew --version | cut -d ' ' -f2)
+    echo "Brew has been updated! Running version: $VERSION"
+    ;;
+
+    "Update a Brew Package(s)")
+    echo "Use SPACE to select, ENTER to Update"
+    UPDATE=$(brew list | gum choose --no-limit)
+    for app in $UPDATE
     do
-      gum spin --spinner dot --title "Updating $p" -- brew upgrade $p
-      echo "Updated $p"
+        gum spin --spinner dot --title "Updating $app" -- brew update $app
+        echo "Updated $app"
     done
-  ;;
-  "Uninstall a Brew Package(s)")
-    echo "Space to select, Enter to uninstall"
-    brew list | gum choose --no-limit | xargs brew uninstall
-  ;;
-  *) gum style "No selection made"
-  ;;
+    ;;
+
+    "Uninstall Package")
+    echo "Press ENTER to Uninstall"
+    TITLE=$(brew list | gum choose --limit 1)
+    gum style --foreground="#539FC5" "You have selected to Uninstall $TITLE."
+
+    if gum confirm; then
+        gum spin --spinner dot --title "Uninstalling $TITLE" -- brew uninstall $TITLE
+    else
+        echo "Uninstall Cancelled"
+    fi
+    ;;
+
+    *) gum style "No selection made"
+    ;;
 esac
